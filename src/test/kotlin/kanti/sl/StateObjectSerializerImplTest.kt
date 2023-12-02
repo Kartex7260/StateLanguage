b3f519598cc25376b68e8654a578d9384bd9ea45
Kirill Zhitko <kirillzhitko@gmail.com>
2023-12-02 14:36:46 +0300

    FEAT: тест для StateObjectSerializerImpl

---
+++ src/test/kotlin/kanti/sl/StateObjectSerializerImplTest.kt
@@ -0,0 +1,46 @@
+package kanti.sl
+
+import kanti.sl.arguments.StateArgument
+import kanti.sl.objects.StateObject
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StateObjectSerializerImplTest {
+
+	private val serializer: StateObjectSerializer = StateObjectSerializer.builder().builder()
+
+	@Test
+	fun serialize() {
+		val stg = serializer.serialize(
+			StateObject.create(
+				name = "Test",
+				args = arrayOf(
+					StateArgument.create("isSuccess", true),
+					StateArgument.create("amount", 100.3)
+				)
+			)
+		)
+		assertEquals(
+			"Test:isSuccess=true,amount=100.3",
+			stg
+		)
+	}
+
+	@Test
+	fun deserialize() {
+		val line = "Test:isSuccess=true,amount=100.3"
+		val obj = serializer.deserialize(line)
+		assertEquals("Test", obj.name)
+
+		val arg1 = obj.arguments["isSuccess"]
+		assertEquals(arg1?.name, "isSuccess")
+		assertEquals(arg1?.value, true)
+
+		val arg2 = obj.arguments["amount"]
+		assertEquals(arg2?.name, "amount")
+		assertEquals(arg2?.value, 100.3)
+	}
+
+}
